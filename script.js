e71c2305fe3fd09265c3b3ca7c317531a12212c6
Aliaksa <sashvero@gmail.com>
2024-06-06 14:59:59 +0300

    init: add my code

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(
+  (
+    "Diner".split("").splice(0, 3).join("") +
+    "from".split("").splice(2, 1).join("") +
+    "soursop".split("").splice(0, 4).join("")
+  )
+    .split("")
+    .join("")
+);
+//
+console.log(typeof "" + " of pearls");
+console.log("Don't " + typeof (1 > 0) + " me");
+console.log(`You can trust me, everything I'm telling you is ${1 > 0}`);
+console.log(`ba${2 / undefined}a`.toLocaleLowerCase());
